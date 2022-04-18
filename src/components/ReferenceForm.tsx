d9f308cc77f57489022df5df225ce1191e6fb6ba
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-04-18 20:19:28 +0100

    setup personal fieldset with form fields

--- src/components/ReferenceForm.tsx
+++ src/components/ReferenceForm.tsx
@@ -1,8 +1,39 @@
+import { useForm, FieldValues } from 'react-hook-form';
+
 type Props = {}
 
 const ReferenceForm = (props: Props) => {
+
+    const {
+        register,
+        handleSubmit,
+    } = useForm({
+        mode: 'all' // validate on blur
+    });
+
+    const doSubmit = (data: FieldValues) => {
+        // console.log(data);
+    }
+
     return (
-        <div>ReferenceForm</div>
+        <form onSubmit={handleSubmit(doSubmit)}>
+
+            <fieldset>
+                <h4>Personal</h4>
+
+                <label htmlFor='firstName'>First Name</label>
+                <input type='text' {...register("firstName")} />
+
+                <label htmlFor='lastName'>Last Name</label>
+                <input type='text' {...register('lastName')} />
+
+                <label htmlFor='address'>Address</label>
+                <input type='text' {...register('address')} />
+            </fieldset>
+
+            <button type="submit">Submit</button>
+
+        </form>
     )
 }
 
