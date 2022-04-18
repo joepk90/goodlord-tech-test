963154d5b416f316a4d2ea1b500a029921ba9d7a
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-04-18 20:36:38 +0100

    setup react-hook-form resolver: yup validation. conditionally add error attribute to field if touched and is invalid. add css class show invalid fields

--- src/components/ReferenceForm.tsx
+++ src/components/ReferenceForm.tsx
@@ -1,4 +1,21 @@
 import { useForm, FieldValues } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from "yup";
+
+const schema = Yup.object({
+    firstName: Yup.string()
+        .min(2, 'Minimum of 2 characters required')
+        .max(50, 'Maximum of 50 characters required')
+        .required('Required'),
+    lastName: Yup.string()
+        .min(2, 'Minimum of 2 characters required')
+        .max(50, 'Maximum of 50 characters required')
+        .required('Required'),
+    address: Yup.string()
+        .min(2, 'Minimum of 2 characters required')
+        .max(50, 'Maximum of 50 characters required')
+        .required('Required')
+}).required();
 
 type Props = {}
 
@@ -7,12 +24,23 @@ const ReferenceForm = (props: Props) => {
     const {
         register,
         handleSubmit,
+        formState
     } = useForm({
+        resolver: yupResolver(schema),
         mode: 'all' // validate on blur
     });
 
+    const {
+        errors,
+        touchedFields
+    } = formState;
+
     const doSubmit = (data: FieldValues) => {
         // console.log(data);
+    }
+
+    const hasError = (property: string) => {
+        return touchedFields[property] && errors[property] ? { 'error': errors[property].message } : null
     }
 
     return (
@@ -22,13 +50,25 @@ const ReferenceForm = (props: Props) => {
                 <h4>Personal</h4>
 
                 <label htmlFor='firstName'>First Name</label>
-                <input type='text' {...register("firstName")} />
+                <input
+                    type='text'
+                    {...register("firstName")}
+                    {...(hasError("firstName"))}
+                />
 
                 <label htmlFor='lastName'>Last Name</label>
-                <input type='text' {...register('lastName')} />
+                <input
+                    type='text'
+                    {...register('lastName')}
+                    {...(hasError("lastName"))}
+                />
 
                 <label htmlFor='address'>Address</label>
-                <input type='text' {...register('address')} />
+                <input
+                    type='text'
+                    {...register('address')}
+                    {...(hasError("address"))}
+                />
             </fieldset>
 
             <button type="submit">Submit</button>
